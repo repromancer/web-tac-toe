4123c260bdc0aa07e9b801e58a8db0f0d192e510
Noah Summers <summers.noah@gmail.com>
2017-07-21 15:23:24 -0400

    repair `has_many/belongs_to` associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,16 @@ class User < ActiveRecord::Base
   has_many :game_user_joins
   has_many :games, through: :game_user_joins
 
-  has_many :won_games, class_name: 'Game', foreign_key: 'winner_id'
-  has_many :lost_games, class_name: 'Game', foreign_key: 'loser_id'
+  has_many :won_games, class_name: 'Game',
+    foreign_key: :winner_id, inverse_of: :winner
 
-  has_many :received_invites, class_name: 'Invite', foreign_key: 'receiver_id'
-  has_many :sent_invites, class_name: 'Invite', foreign_key: 'sender_id'
+  has_many :lost_games, class_name: 'Game',
+    foreign_key: :loser_id, inverse_of: :loser
+
+  has_many :received_invites, class_name: 'Invite',
+    foreign_key: :receiver_id, inverse_of: :receiver
+
+  has_many :sent_invites, class_name: 'Invite',
+    foreign_key: :sender_id, inverse_of: :sender
+
 end
