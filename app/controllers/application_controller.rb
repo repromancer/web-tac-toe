6437c51bda7ab40ed47f973a326e87a1bc18d580
Noah Summers <summers.noah@gmail.com>
2017-07-22 01:27:38 -0400

    add basic page protection filter to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,23 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+
+    # NEEDS PERSISTENT SECRET
+    # set :session_secret, ENV.fetch('SESSION_SECRET') { Sysrandom.hex(64) }
+    enable :sessions
+
+    use Rack::Flash
+  end
+
+  helpers AuthenticationUtils
+
+  before /\/(?!signup|login).+/ do
+    binding.pry
+    screen_unauthorized_users
   end
 
   get "/" do
-    erb :welcome
+    erb :'index.html'
   end
 
 end
