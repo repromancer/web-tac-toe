5362ca4ce48e25d708020fda3646beb244060788
Noah Summers <summers.noah@gmail.com>
2017-07-27 02:06:40 -0400

    make delete route for invites more semantic

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -21,14 +21,17 @@ class InvitesController < ApplicationController
   end
 
 
-  delete '/invites' do
-    Invite.find(params[:invite_id]).tap do |invite|
+
+  delete '/invites/:id' do
+
+    Invite.find(params[:id]).tap do |invite|
       if invite.sender == current_user
         invite.delete
       end
     end
 
     redirect "/users/#{current_user.slug}"
+
   end
 
   patch '/invites' do
